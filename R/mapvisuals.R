e0a270ef7e68eb75eba3a02b47c0fbe6f70b49bd
Christopher Haid <chaid@kippchicago.org>
2014-02-06 15:16:10 -0600

    Added mapvisuals.R so we can manage the Rosxygen2 creation of NAMESPACE. Doing so also added mapvisuals.Rd to /man.  Updated NAMESPACE.

---
+++ R/mapvisuals.R
@@ -0,0 +1,13 @@
+#' The mapvisuals package.
+#'
+#' @docType package
+#' @name mapvisuals
+#' @title  KIPP MAP Visuals
+#' @author Andrew Martin
+#' @author Chris Haid
+#' @description Functions that support analysis of NWEA MAP assessment data
+#' @import assertthat
+#' @import data.table
+#' @import ggplot2
+#' 
+NULL
