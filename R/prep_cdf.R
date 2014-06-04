2ee1580147b958b36fc698bcca1bfd060bee54ba
unknown <amartin@RM9-AMartin1.teamschools.kipp.org>
2014-06-04 17:03:45 -0400

    prep_cdf to use new functions

--- R/prep_cdf.R
+++ R/prep_cdf.R
@@ -1,16 +1,17 @@
 #' @title Prep CDF 
 #'
 #' @description
-#' \code{prep_cdf} is a utility function that preps a standard NWEA MAP CDF for use with MAPVizier
-#'
+#' \code{prep_cdf} is a wrapper around \code{term_fallwinterspring} and \code {term_map_year_academic}
+#' 
 #' @param regular_cdf a NWEA MAP cdf loaded in as a data frame
 #' 
 #' @export
 
 prep_cdf <- function(regular_cdf) {
  
-  #split termnames to get fallwinterspring by itself
-  prepped_cdf <- term_split(regular_cdf)
-  return(prepped_cdf)
+  #split
+  regular_cdf$fallwinterspring <- term_fallwinterspring(regular_cdf$termname)
+  regular_cdf$map_year_academic <- term_map_year_academic(regular_cdf$termname)
   
+  return(regular_cdf) 
 }
