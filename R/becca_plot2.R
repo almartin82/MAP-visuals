fd0e185ae2f165c586ef8e43c714d91889d921d2
Chris Haid <chaid@kippchicago.org>
2014-07-01 09:19:56 -0500

    Added validation to becca_plot2.mapvizier() to check that the subsetting function is included. Updated documentation for becca_plot2() generic.

--- R/becca_plot2.R
+++ R/becca_plot2.R
@@ -1,33 +1,33 @@
 #' @title becca_plot2
 #'
-#' @description method as wrapper for \code{\link{becca_plot}}
+#' @description \code{becca_plot2()} is a method that serves as wrapper for \code{\link{becca_plot}}.
+#' 
 #'
-#' @details See \code{\link{becca_plot}}
-
-#' @param x an object to plot
+#' @details \code{becca_plot2} serves as wrapper that dispatches to \code{\link{becca_plot}} depending
+#' on the class of the object passed to the \code{.data} parameter. 
+#' 
+#' If the object is a \code{\link{mapvizier}} object, then \code{becca_plots} requirement that the user
+#' identify a litany of columns in the data is unnecessary (since we know which 
+#' columns are which in a \code{mapvizier object}). The \code{subsetting} argument must be used
+#' when a \code{mapvizier} object is passed, since the data will need to be 
+#' properly subsetted. The user should pass this argument a call to \code{\link[dplyr]{filter(...)}}, where 
+#' that function's \code{.data} argument is unnecessary. All other arguments (e.g. \code{facets})
+#' are passed to \code{becca_plot} via the \code{...} argument.
+#' 
+#' Ff the object is a not a \code{mapvizier} object (like a \code{data.frame}) then as with \code{becca_plot}, the 
+#' user needs to identify school, cohort, year, grade-level-season, and percentile columns.
+#' 
+#' @seealso \code{\link{becca_plot}}, \code{link[dplyr]{filter}}
+#' @param .data an object to plot
 #' @param ... other arguments passed to \code{\link{becca_plot}}
+#' @param subsetting (required for \code{mapvizier obejcts}) a call to \code{dplyr}'s \code{filter} function, leavingout 
+#' the \code{.data} argument (i.e. only containing subsetting arguments)
 
 #' @return a \code{link[ggplot2]{ggplot2}} object
 #' @rdname becca_plot2
 #' @export
-becca_plot2 <- function(.data
-#                         ,school_name_column = 'sch_abbr'
-#                         ,cohort_name_column = 'cohort'
-#                         ,academic_year_column = 'map_year_academic'
-#                         ,grade_level_season_column = 'grade_level_season'
-#                         ,measurement_scale_column = 'measurementscale'
-#                         ,percentile_column = 'percentile_2011_norms'
-#                         ,first_and_spring_only = TRUE
-#                         ,auto_justify_x = TRUE
-#                         ,justify_widths = FALSE
-#                         ,justify_min = NA
-#                         ,justify_max = NA
-#                         ,entry_grades = c(-0.7, 4.3)
-#                         ,color_scheme = 'KIPP Report Card'
-#                         ,facets = FALSE
-#                         ,facet_opts = FALSE
-#                         ,title_text = FALSE
-                        ,...) {UseMethod("becca_plot2")}
+becca_plot2 <- function(.data, ...) UseMethod("becca_plot2")
+
 
 #' @rdname becca_plot2.default
 becca_plot2.default <- function(.data
@@ -82,6 +82,14 @@ becca_plot2.mapvizier <- function(.data, ...
                                                     MeasurementScale %in% c("Reading", "Mathematics")
                                   )){
 
+  # check to see if subsetting argument is missing
+  if(missing(subsetting)){ 
+    stop(paste("mapvizier objects must be including a subsetting argument.\n",
+               "You should pass something like subsetting=filter(CohortYear==2018).\n",
+               "Type ?becca_plot2 or ?filter for more information."
+               )
+    )
+  }
     # Extract mapData from mapvizier object
    .data<-as.data.frame(.data$mapData) 
  
